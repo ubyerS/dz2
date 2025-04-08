a1234b9fc6bff15e121d1b140fb42f0346971aa0
ubyerS <125369545+ubyerS@users.noreply.github.com>
2025-04-09 01:33:30 +0300

    feat: light icons for dark theme

--- src/Game/GameApp.tsx
+++ src/Game/GameApp.tsx
@@ -4,8 +4,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { ScoreState } from '../Store/Store';
 import { useTheme } from '../ThemeChanger/ThemeChanger';
 import paperImage from '../images/paper_black.svg';
+import whitePaperImage from '../images/paper_white.svg';
 import rockImage from '../images/rock_black.svg';
+import whiteRockImage from '../images/rock_white.svg';
 import scissorsImage from '../images/scissors_black.svg';
+import whiteScissorsImage from '../images/scissors_white.svg';
 
 enum GameResult {
   win = 'YOU WON ! 🎉',
@@ -15,10 +18,16 @@ enum GameResult {
 
 type Choice = 'rock' | 'paper' | 'scissors';
 
-const choiceImages = {
+const choiceDarkImages = {
   rock: rockImage,
   paper: paperImage,
   scissors: scissorsImage,
+};
+
+const choiceLightImages = {
+  rock: whiteRockImage,
+  paper: whitePaperImage,
+  scissors: whiteScissorsImage,
 };
 
 function GameApp() {
@@ -68,67 +77,97 @@ function GameApp() {
       >
         {theme === 'light' ? 'dark' : 'light'}
       </button>
-      <h1 className="font-bold text-3xl text-blue-950 mb-8">Rock Paper Scissors</h1>
-      <button onClick={handleReset} className="font-bold text-xl mb-4">
+      <h1 className="font-bold text-3xl text-blue-950 mb-8 dark:text-[#BEBFD1]">
+        Rock Paper Scissors
+      </h1>
+      <button onClick={handleReset} className="font-bold text-xl mb-4 dark:text-white">
         RESET THE SCORE
       </button>
       <div className="flex justify-between gap-x-[143px] mb-8">
         <div className="Player-score">
-          <h2 className="font-semibold text-[14px]">PLAYER SCORE: {personScore}</h2>
+          <h2 className="font-semibold text-[14px] dark:text-white">
+            PLAYER SCORE: {personScore}
+          </h2>
         </div>
 
         <div className="Computer-score">
-          <h2 className="font-semibold text-[14px]">COMPUTER SCORE: {computerScore}</h2>
+          <h2 className="font-semibold text-[14px] dark:text-white">
+            COMPUTER SCORE: {computerScore}
+          </h2>
         </div>
       </div>
 
       <div className="mb-8">
-        <h2 className="font-bold text-[40px] text-red-900"> {gameResult} </h2>
+        <h2 className="font-bold text-[40px] text-red-900 dark:text-[#6881D8]">
+          {gameResult}
+        </h2>
       </div>
 
       <div className="gap-x-12">
         <div className="flex justify-center items-center mb-8 gap-x-14.5">
           <div>
-            <img
-              src={choiceImages[playerChoice]}
-              alt={playerChoice}
-              className="icon transform scale-x-[-1]"
-            />
+            {playerChoice ? (
+              <div>
+                <img
+                  src={
+                    theme === 'dark'
+                      ? choiceLightImages[playerChoice]
+                      : choiceDarkImages[playerChoice]
+                  }
+                  alt={playerChoice}
+                  className="transform scale-x-[-1]"
+                />
+              </div>
+            ) : (
+              <div className="w-[100px] h-[100px]"></div>
+            )}
           </div>
-          <div className="font-bold text-xl">VS</div>
+
+          <div className="font-bold text-xl dark:text-white">VS</div>
+
           <div>
-            <img
-              src={choiceImages[computerChoice]}
-              alt={computerChoice}
-              className="comp"
-            />
+            {computerChoice ? (
+              <div>
+                <img
+                  src={
+                    theme === 'dark'
+                      ? choiceLightImages[computerChoice]
+                      : choiceDarkImages[computerChoice]
+                  }
+                  alt={computerChoice}
+                  className="comp"
+                />
+              </div>
+            ) : (
+              <div className="w-[100px] h-[100px]"></div>
+            )}
           </div>
         </div>
 
-        <h2 className="flex mb-6 justify-center">
+        <h2 className="flex mb-6 justify-center dark:text-white">
           Choose your move, rock paper or scissors:
         </h2>
 
         <div className="flex gap-x-12">
           <button
             onClick={() => handleChoice('rock')}
-            className="w-42.5 h-15 text-white text-[25px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center"
+            className="w-42.5 h-15 text-white text-[25px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center
+            dark:bg-[#2751A3]"
           >
-            {' '}
             ROCK
           </button>
           <button
             onClick={() => handleChoice('paper')}
-            className="w-42.5 h-15 text-white text-[25px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center"
+            className="w-42.5 h-15 text-white text-[25px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center
+            dark:bg-[#2751A3]"
           >
-            {' '}
             PAPER
           </button>
           <button
             onClick={() => handleChoice('scissors')}
-            className="w-42.5 h-15 text-white text-[25px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center"
+            className="w-42.5 h-15 text-white text-[25px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center
+            dark:bg-[#2751A3]"
           >
-            {' '}
             SCISSORS
           </button>
         </div>
--- src/StartScreen/StartScreen.tsx
+++ src/StartScreen/StartScreen.tsx
@@ -17,13 +17,18 @@ function StartScreen() {
       <div className="flex items-center">
         <button
           onClick={() => navigate('/game')}
-          className="mr-8 w-42 h-22.5 text-white text-[27px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center"
+          className="mr-8 w-42 h-22.5 text-white text-[27px] font-bold bg-red-500 aspect-square rounded-[14px] place-content-center
+          dark:bg-[#2751A3]"
         >
           PLAY
         </button>
         <div>
-          <h1 className="text-3xl text-blue-950 font-bold">Rock Paper Scissors</h1>
-          <h1 className="text-3xl text-blue-950 font-bold">The game</h1>
+          <h1 className="text-3xl text-blue-950 font-bold dark:text-[#BEBFD1]">
+            Rock Paper Scissors
+          </h1>
+          <h1 className="text-3xl text-blue-950 font-bold dark:text-[#BEBFD1]">
+            The game
+          </h1>
         </div>
       </div>
     </div>
