931bce34f11d6fc35638dd6951f92e3134a1459d
ubyerS <125369545+ubyerS@users.noreply.github.com>
2025-04-10 10:34:34 +0300

    feat: component ThemeSwitch.tsx

---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,20 @@
+import darkMode from '../images/dark_mode.svg';
+import lightMode from '../images/light_mode.svg';
+import { useTheme } from './ThemeChanger.tsx';
+
+export function ThemeSwitch() {
+  const { theme, handleThemeChange } = useTheme();
+
+  return (
+    <button
+      onClick={handleThemeChange}
+      className="absolute top-8 right-8 text-3xl p-2 hover:scale-110 transition-transform dark:text-white"
+    >
+      {theme === 'light' ? (
+        <img src={darkMode} alt={'dark mode'} />
+      ) : (
+        <img src={lightMode} alt={'light mode'} />
+      )}
+    </button>
+  );
+}
